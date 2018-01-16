2af1b217cf192852b97cdc09b7c83b5328b008b6
Riceamf <2433240583@qq.com>
2018-01-16 12:23:42 +0800

    "修改TransItem;TestStringToBoll2表示从以字符串表示的逻辑表达式转换为逻辑判别结果"

---
+++ GraduationProject/src/transition/TestStringToBoll2.java
@@ -0,0 +1,32 @@
+package transition;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class TestStringToBoll2 {
+
+	public static void main(String[] args) {
+
+		String str1 = "x";
+		String str2 = ">= 2";
+		System.out.println(getBooleanFromString(str1, str2));
+
+	}
+
+	public static boolean getBooleanFromString(String str1, String str2) {
+		String str = str1 + str2;
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("js");
+		Object result = null;
+		try {
+			engine.put("x", 6);
+			result = engine.eval(str);
+		} catch (ScriptException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("结果类型:" + result.getClass().getName() + ",计算结果:" + result);
+		return (boolean) result;
+	}
+}
--- GraduationProject/src/transition/TransItem.java
+++ GraduationProject/src/transition/TransItem.java
@@ -2,11 +2,17 @@ package transition;
 
 import java.util.Set;
 
+/**
+ * https://github.com/Ricesmf/graduationProject
+ * 
+ */
 public class TransItem {
 
 	private String relationStr; // 前序关系等
 	private String duration; // 表示转换完成的时间区间；例如[1,5] (1,5]等
 	public boolean guard; // 以relationStr和guard共同决定转换是否可以发生，路径可行与否
+	public int times;
+	public double probability;
 	public Set<Integer> itemToBeCleared;// 集合中元素是时钟元素；表示需要清零重新计时的时钟元素
 
 	public String getRelationStr() {
@@ -39,6 +45,12 @@ public class TransItem {
 
 	public void setItemToBeCleared(Set<Integer> itemToBeCleared) {
 		this.itemToBeCleared = itemToBeCleared;
+	}
+
+	public TransItem(String relationStr, boolean guard) {
+		super();
+		this.relationStr = relationStr;
+		this.guard = guard;
 	}
 
 }
